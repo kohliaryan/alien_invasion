8d9d6c347e10f615efccd7772e376277cd05105d
Aryan Kohli <110827474+kohliaryan@users.noreply.github.com>
2023-01-08 20:55:56 +0530

    Add files via upload

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        while True:
+            self._check_events()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                 sys.exit()
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blithe()
+
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
